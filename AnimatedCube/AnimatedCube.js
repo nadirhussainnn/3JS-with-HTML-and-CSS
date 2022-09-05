d8e74b9a0f21099f644aa5783c482793da6f1e64
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-09-05 01:52:40 -0700

    created animated cube

---
+++ AnimatedCube/AnimatedCube.js
@@ -0,0 +1,70 @@
+import * as THREE from "three";
+
+function main() {
+  const canvas = document.querySelector("#basic-canvas");
+  const renderer = new THREE.WebGLRenderer({ canvas });
+
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(
+    fov,
+    canvas.clientWidth / canvas.clientHeight,
+    near,
+    far
+  );
+  camera.position.z = 2;
+
+  const scene = new THREE.Scene();
+
+  //Adding directional lighting to Scene
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 }); // greenish blue: We changed mesh to show lightening
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  renderer.render(scene, camera);
+
+  function render(time) {
+    time *= 0.001; // convert time to seconds
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  resizeRendererToDisplaySize(renderer);
+}
+
+main();
+
+//Making canvas responsive : That canvas defaults to 300x150 CSS pixels in size. so we set it's height and width in css
