4a743f025f6bef95e5b6aa6ab211cb76db5017ec
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-09-05 05:25:08 -0700

    updated

--- AnimatedEarth/earth.js
+++ AnimatedEarth/earth.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"; //three/addons is added in HTMl with three in importmaps
 
 function main() {
   const canvas = document.querySelector("canvas");
-  const renderer = new THREE.WebGLRenderer({ canvas });
-
+  const renderer = new THREE.WebGLRenderer({ canvas, logarithmicDepthBuffer: true });
+  renderer.shadowMap.enabled = true;
+  
   const fov = 40;
-  const aspect = 2; // the canvas default
   const near = 0.1;
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(
@@ -14,8 +15,12 @@ function main() {
     near,
     far
   );
+
   camera.position.z = 120;
 
+  //Other camera's
+
+  
   const scene = new THREE.Scene();
 
   //Adding directional lighting to Scene
@@ -27,16 +32,25 @@ function main() {
     scene.add(light);
   }
 
-  const radius = 10;  // ui: radius
-  const detail = 2;  // ui: detail
+  const radius = 10; // ui: radius
+  const detail = 2; // ui: detail
   const geometry = new THREE.DodecahedronGeometry(radius, detail);
-  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88, flatShading:true, side:THREE.DoubleSide });
-  //important property: flatShading:true
 
+  //Adding Textures i.e Image, text to Material
+  const loader = new THREE.TextureLoader(); //Remove material color while adding texture
+
+  const material = new THREE.MeshPhongMaterial({
+    color: 0x44aa88,
+    flatShading: true,
+    side: THREE.DoubleSide,
+    // map: loader.load(
+    //   `https://images.unsplash.com/profile-1441298803695-accd94000cac?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128&s=32085a077889586df88bfbe406692202`
+    // ),
+  });
+  //important property: flatShading:true
 
   const earth = new THREE.Mesh(geometry, material);
   scene.add(earth);
-
   renderer.render(scene, camera);
 
   function render(time) {
@@ -44,14 +58,16 @@ function main() {
 
     earth.rotation.x = time;
     earth.rotation.y = time;
-    earth.rotation.z=time
+    earth.rotation.z = time;
 
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
   }
+
   requestAnimationFrame(render);
 
+  //To resolve shape result issue
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
@@ -64,10 +80,49 @@ function main() {
   }
 
   resizeRendererToDisplaySize(renderer);
+  scene.background = new THREE.Color(0x000000);
 
-  //customizing scene
+  //adding user inputs on Earth, so import {OrbitControls} from '/examples/jsm/controls/OrbitControls.js';
+  //   camera.position.set(0, 10, 20);
 
-  scene.background=new THREE.Color(0xAAAAAA);
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0); //displace the target from origin in y:5 direction
+  controls.update(); //Repaint the DOM
+
+  //#region Working with differnt lights
+
+  //   const color = 0xffffff;
+  //   const intensity = 1;
+  //   const light = new THREE.AmbientLight(color, intensity);
+  //   scene.add(light);
+
+  //Let's add sky and earth color: So use HemisphereLight
+  // A HemisphereLight takes a sky color and a ground color and just multiplies the material's color between those 2 colors—the sky color if the surface of the object is pointing up and the ground color if the surface of the object is pointing down.
+
+  // const skyColor = 0xB1E1FF;  // light blue
+  // const groundColor = 0xB97A20;  // brownish orange
+  // const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+  // scene.add(light);
+
+  // Let's switch the code to a DirectionalLight. A DirectionalLight is often used to represent the sun.
+
+  const color = 0xffffff;
+  const intensity = 0.5;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(0, 10, 0);
+  light.target.position.set(-5, 0, 0);
+  scene.add(light);
+  scene.add(light.target);
+
+  light.castShadow = true;
+  
+  //Shadows
+  const shadowTexture = loader.load('./round-shadow.jpg');
+
+//   const yOff = Math.abs(Math.sin(time * 2 + ndx));
+//     // move the sphere up and down
+//     sphereMesh.position.y = y + THREE.MathUtils.lerp(-2, 2, yOff);
+
 }
 
 main();
